e7fc05f0f127b5a1c74f9dc2276e48b331ad3e4d
Marvin Bechtold <marvin.bechtold@gmail.com>
2020-06-08 18:13:28 +0200

    use sha256 as image key

--- image_recognition/count_people.py
+++ image_recognition/count_people.py
@@ -1,9 +1,8 @@
-
 import boto3
+import hashlib
 
-IMAGE = "/Users/marvin/abgabenUniGit/aws-rekognition/supermarket.jpg"
+IMAGE = "images/image_1.png"
 BUCKET = "amazon-rek"
-KEY = "supermarket"
 
 def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="us-east-1"):
 	rekognition = boto3.client("rekognition", region)
@@ -19,10 +18,28 @@ def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="us-east
 	)
 	return response['Labels']
 
-# s3 = boto3.client('s3')
-# with open(IMAGE, "rb") as f:
-#     s3.upload_fileobj(f, BUCKET, KEY)
+def generate_key(filename):
+	sha256_hash = hashlib.sha256()
+	with open(filename,"rb") as f:
+		# Read and update hash string value in blocks of 4K
+		for byte_block in iter(lambda: f.read(4096),b""):
+			sha256_hash.update(byte_block)
+	return sha256_hash.hexdigest()
 
-response_labels = detect_labels(BUCKET, KEY)
-persons = [element for element in response_labels if element['Name'] == 'Person'][0]
-print(len(persons['Instances']))
+
+def upload_image(image, bucket, key):
+	s3 = boto3.client('s3')
+	with open(image, "rb") as f:
+		s3.upload_fileobj(f, bucket, key)
+
+def get_number_of_persons(response):
+	persons = [element for element in response_labels if element['Name'] == 'Person'][0]
+	return len(persons['Instances'])
+
+
+key = generate_key(IMAGE)
+print(f'Key : {key}')
+upload_image(IMAGE, BUCKET, key)
+response_labels = detect_labels(BUCKET, key)
+print(get_number_of_persons(response_labels))
+
