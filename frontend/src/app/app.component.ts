d54f8026d46d6914adc477a3e74e5583098d7b6c
Thomas Wangler <thomas.wangler1996@web.de>
2020-06-20 16:38:38 +0200

    sidenav displays component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { ShopsService } from './services/shops.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showHideDisplays', [
+      // ...
+      state('open', style({
+        height: '*',
+        overflow: 'hidden',
+        opacity: 1,
+      })),
+      state('closed', style({
+        height: '0px',
+        overflow: 'hidden',
+      })),
+      transition('open => closed', [
+        animate('0.8s')
+      ]),
+      transition('closed => open', [
+        animate('0.4s')
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'frontend';
+  showDisplays = false
+  constructor(public shops: ShopsService) {
+  }
+
+  toggleDisplays() {
+    this.showDisplays = !this.showDisplays
+  }
 }
