2be3a80d4980c3cce3214f7512b3efaad274cd04
Thomas Wangler <thomas.wangler1996@web.de>
2020-07-03 15:52:15 +0200

    logo

--- frontend/src/app/next-shop/next-shop.component.ts
+++ frontend/src/app/next-shop/next-shop.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ShopsService } from '../services/shops.service';
 import { Shop } from '../utility/shop';
 
+
 @Component({
   selector: 'app-next-shop',
   templateUrl: './next-shop.component.html',
@@ -9,18 +10,21 @@ import { Shop } from '../utility/shop';
 })
 export class NextShopComponent implements OnInit {
   nextShop: Shop;
-
+  logoSrc: string;
   constructor(public shops: ShopsService) { }
 
   async ngOnInit() {
     if (this.shops.shopsSelected) {
       let nextShop = await this.shops.getNextShop();
       this.nextShop = nextShop;
+      this.logoSrc = nextShop.logo;
     }
   }
 
   shopVisited() {
-
+    this.shops.shopVisited()
   }
 
 }
+
+
