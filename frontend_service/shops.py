33f062473b2e22f7ee79b24cd4d586e4f8d9d12d
Maximilian Reichel <reichemn@icloud.com>
2020-07-03 16:56:23 +0200

    history grouping

--- frontend_service/shops.py
+++ frontend_service/shops.py
@@ -75,13 +75,19 @@ class ShopHistoricalPeopleDataApi(Resource):
                        {
                 '$group': {
                     '_id': {
-                        'timestamp': {'$subtract': [{'$divide': ['$timestamp', intervall_seconds]}, {'$mod': [{'$divide': ['$timestamp', intervall_seconds]}, 1]}]}
+                        'timestamp': {'$multiply': [{'$subtract': [{'$divide': ['$timestamp', intervall_seconds]}, {'$mod': [{'$divide': ['$timestamp', intervall_seconds]}, 1]}]}, intervall_seconds]}
                     },
                     'count': {'$avg': '$payload.count'},
-                    'timestamp': {'$first': '$timestamp'},
                     'shop_id': {'$first': '$payload.shop_id'},
                     'datetime': {'$first': '$datetime'}
                 }
+            }, {'$project': {
+                '_id': 0,
+                'timestamp': '$_id.timestamp',
+                'datetime': 1,
+                'payload.count': '$count',
+                'payload.shop_id': '$shop_id'
+            }
             }]
             data = list(MqttData.objects().aggregate(pipline))
         except Exception as e:
@@ -105,5 +111,5 @@ def get_newest_topic_data(topic_regex):
     return list(MqttData.objects().aggregate(pipline))
 
 def filter_mqtt_data(mqtt_data):
-    relevant_keys = ["count", "timestamp", "datetime", "shop_id"]
+    relevant_keys = ["payload", "timestamp", "datetime"]
     return { key: mqtt_data[key] for key in relevant_keys }
